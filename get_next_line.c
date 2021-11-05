7964f1e149845a06ff550c0cbd7b98059cafd2f7
sdxsd <willmaguire@icloud.com>
2021-11-05 12:06:09 +0100

    Trying to figure out how this will work

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,34 @@
 /*   By: wmaguire <wmaguire@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 15:58:01 by wmaguire      #+#    #+#                 */
-/*   Updated: 2021/11/04 16:02:38 by wmaguire      ########   odam.nl         */
+/*   Updated: 2021/11/05 12:05:25 by wmaguire      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+static char	*get_newline(int size, void *buf)
+{
+	;
+}
 
 char	*get_next_line(int fd)
 {
-	static int	F_MARK;
-	if ()
-	{
-		
-	}
-	else
+	static int	f_mark;
+	char		*line;
+	void		*buf;
+	size_t		read_c;
+
+	if (fd == -1)
 		return (NULL);
+	buf = malloc(sizeof(char) * BUFFER_SIZE);
+	read_c = read(fd, buf, BUFFER_SIZE);
+	if (read_c == READ_ERROR)
+		return (READ_ERROR);
+	if (read_c == EOF)
+		get_newline(BUFFER_SIZE, buf);
+	return (get_newline(read_c, buf));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: wmaguire <wmaguire@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/04 15:58:14 by wmaguire      #+#    #+#                 */
-/*   Updated: 2021/11/04 15:58:15 by wmaguire      ########   odam.nl         */
+/*   Updated: 2021/11/05 11:23:06 by wmaguire      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 # define GET_NEXT_LINE_H
 # define TRUE 1
 # define FALSE 0
+# define READ_ERROR -1
+# define EOF 0
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 32
+# endif
 
 /*
 * Þe funktion get_next_line() af krïg fōrgenden līne
