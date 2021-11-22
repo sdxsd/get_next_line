683ae4fbb3b4c86cd34bd0b6f36dbaee6c2762c6
sdxsd <willmaguire@icloud.com>
2021-11-22 16:40:51 +0100

    Rewriting once again from scratch

--- get_next_line.c
+++ get_next_line.c
@@ -13,21 +13,58 @@
 #include "get_next_line.h"
 #include <unistd.h>
 
+static size_t	read_data(void	*buf, int fd)
+{
+	size_t	bytes_read;
+
+	bytes_read = read(fd, buf, BUFFER_SIZE);
+	return (bytes_read);
+}
+
+static char	*gnl_getline(void *buf, size_t bufsize)
+{
+	char	*c_buf;
+	char	*line;
+	size_t	iterator;
+
+	c_buf = (char *)buf;
+	iterator = 0;
+	while (c_buf[iterator] != '\n')
+	{
+		if (iterator >= bufsize)
+			break ;
+		iterator++;
+	}
+	line = gnl_strndup(c_buf, iterator);
+	return (line);
+}
+
+static char	*gnl_remainder(void *buf, size_t remainder_len)
+{
+	char	*remainder;
+
+	remainder = gnl_strndup((char *)buf, remainder_len);
+	return (remainder);
+}
+
+static char	*join_remainder(char *cur_rem, char *new_rem)
+{
+	char	*merged;
+
+	merged = gnl_strjoin(cur_rem, new_rem);
+	free(cur_rem);
+	free(new_rem);
+	if (!merged)
+		return (NULL);
+	return (merged);
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*leftover;
-	int			bytes_read;
-	int			bytes_left;
+	static char	*remainder;
+	size_t		bytes_read;
 	void		*buf;
 
-	bytes_read = 0;
-	buf = malloc(sizeof(char) * BUFFER_SIZE);
-	if (!buf)
-		return (NULL);
-	bytes_read = read(fd, buf, BUFFER_SIZE);
-	if (bytes_read < 0)
-		return (NULL);
-	bytes_left = (bytes_read - BUFFER_SIZE);
-	leftover = gnl_strndup((char *)&buf[bytes_read], bytes_left);
-	return (bytes_left);
+	bytes_read = read_data(buf, fd);
+	gnl_getline();
 }
