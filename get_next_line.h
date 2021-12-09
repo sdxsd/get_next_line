22bcab858e40e75a04ff698c90e98d39f73fa7c6
sdxsd <willmaguire@icloud.com>
2021-12-09 17:31:19 +0100

    Added Licensing Information

--- get_next_line.h
+++ get_next_line.h
@@ -10,6 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+GET_NEXT_LINE (An implementation of get_next_line for Codam)
+Copyright (C) 2021  Will Maguire
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+*/
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define TRUE 1
